2fccca9153a31d450eaef02a58200405dddb996e
Mateusz Bednarski <echelon133@gmail.com>
2019-04-10 15:45:50 +0200

    Add RequiredParameterNotGivenException

---
+++ src/main/java/ml/echelon133/services/graphpathfinder/path/RequiredParameterNotGivenException.java
@@ -0,0 +1,8 @@
+package ml.echelon133.services.graphpathfinder.path;
+
+public class RequiredParameterNotGivenException extends Exception {
+
+    public RequiredParameterNotGivenException(String message) {
+        super(message);
+    }
+}
