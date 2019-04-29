f6c2747ea09e4c18e51849a3b313856e2ba556ee
Mateusz Bednarski <echelon133@gmail.com>
2019-04-29 12:23:23 +0200

    Implement full functionality of PathController's method calcPath

--- src/main/java/ml/echelon133/services/graphpathfinder/path/PathController.java
+++ src/main/java/ml/echelon133/services/graphpathfinder/path/PathController.java
@@ -1,10 +1,17 @@
 package ml.echelon133.services.graphpathfinder.path;
 
+import ml.echelon133.graph.Vertex;
+import ml.echelon133.graph.VertexResult;
+import ml.echelon133.services.graphpathfinder.path.exception.RequiredParameterNotGivenException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/graphs/{id}")
@@ -12,10 +19,15 @@ public class PathController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PathController.class);
 
-    public PathController() {}
+    private PathService pathService;
+
+    @Autowired
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
 
     @PostMapping("/paths")
-    public ResponseEntity<String> calcPaths(@PathVariable String id, @RequestParam(required = false) String startFrom) throws Exception {
+    public ResponseEntity<Map<Vertex<BigDecimal>, VertexResult<BigDecimal>>> calcPath(@PathVariable String id, @RequestParam(required = false) String startFrom) throws Exception {
 
         // startFrom param 'required' set to false, so that we can handle its content our own way
         if (startFrom == null || startFrom.isBlank() || startFrom.isEmpty()) {
@@ -23,6 +35,9 @@ public class PathController {
             throw new RequiredParameterNotGivenException("Parameter 'startFrom' is required to proceed with the request");
         }
 
-        return new ResponseEntity<>("test", HttpStatus.OK);
+        Map<Vertex<BigDecimal>, VertexResult<BigDecimal>> result = pathService.calculateShortestPath(id, startFrom);
+
+        LOGGER.debug(String.format("About to return calculated paths of a graph with ID %s (startFrom=%s)", id, startFrom));
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
